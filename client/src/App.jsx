40b72704ada01c654f427411488302cc3fe6fd39
raj075512 <raj075512@gmail.com>
2023-04-23 18:39:20 +0530

    adding the open ai tag or header section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,21 @@
-import React from 'react'
-import {logo} from "./assets";
-import {home,CreatePost} from "./pages"
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import { logo } from "./assets";
+import { home, CreatePost } from "./pages";
+import { BrowserRouter, Link } from "react-router-dom";
 
-const  App=()=> {
+const App = () => {
   return (
-  <BrowserRouter>
-    <header className='w-full flex justify-between items-center bg-white
-    sm:px-8 py-4 border-b border-b-[grey]'>
-     <img src="" alt="" />
-    </header>
-  </BrowserRouter>
-  )
-}
+    <BrowserRouter>
+      <header
+        className="w-full flex justify-between items-center bg-white
+        sm:px-8 py-4 border-b border-b-[grey]"
+      >
+        <Link to="/">
+          <img src={logo} alt="logo" className="w-28 object-contain" />{" "}
+        </Link>
+      </header>
+    </BrowserRouter>
+  );
+};
 
 export default App;
-
--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -9,3 +9,4 @@ const CreatePost = () => {
 }
 
 export default CreatePost
+
