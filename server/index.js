f72c876af30722895dcbc11974636df9919ec041
raj075512 <raj075512@gmail.com>
2023-04-24 18:18:36 +0530

    adding the backend dependencies

---
+++ server/index.js
@@ -0,0 +1,10 @@
+import express from "express"
+import *as dotenv from "dotenv"
+ import cors from "cors"
+
+ dotenv.config();
+ const app =express();
+
+ //middleware //
+ app.use(cors());
+app.use(express.json)
