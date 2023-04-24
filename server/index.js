ab30ab1619321265d17c29e1079135a744834d5f
raj075512 <raj075512@gmail.com>
2023-04-24 18:35:42 +0530

    backend started on port 8080

--- server/index.js
+++ server/index.js
@@ -1,10 +1,29 @@
-import express from "express"
-import *as dotenv from "dotenv"
- import cors from "cors"
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import * as dotenv from "dotenv";
+const port = 8080;
 
- dotenv.config();
- const app =express();
+dotenv.config();
 
- //middleware //
- app.use(cors());
-app.use(express.json)
+const app = express();
+
+//middleware //
+
+app.use(cors());
+
+app.use(express({ limit: "50mb" })); // middleware limits //
+
+app.get("/", async (req, res) => {
+  res.send("hello backend");
+});
+
+
+const serverStart = async () => {
+  app.listen(port,async()=>{
+    console.log(`the backend has been started on port ${port} `)
+  })
+};
+
+
+serverStart();
