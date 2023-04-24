3a99146365cb8ec1ebd53f4f757060ed4dae4b9d
raj075512 <raj075512@gmail.com>
2023-04-24 18:07:41 +0530

    extra code deleted

--- client/src/Components/FormField.jsx
+++ client/src/Components/FormField.jsx
@@ -5,7 +5,7 @@ handleChange,isSurpriseMe ,handleSurpriseMe}) => {
   return (
     <div>
       <div className='flex items-center gap-2 mb-2' >
-           <label htmlFor={name} className='block text-sm  font-medium text-gray-900'>{labelName}
+           <label htmlFor={name} className='block text-sm w-full  font-medium text-gray-900'>{labelName}
               {isSurpriseMe && (
                 <button type='button'onClick={handleSurpriseMe}
                 className=' text-[8px] text-[white] bg-[#09900b]  px-2 mx-2 rounded-[3px] ' >
@@ -20,7 +20,8 @@ handleChange,isSurpriseMe ,handleSurpriseMe}) => {
                id={name}
                onChange={handleChange}
               required
-              className='bg-[#dbd8d8] mt-2 outline-none flex rounded-[5px] border border-gray-300 text-ellipsis text-[12px] w-full  px-2 '
+              className='flex mt-2 justify-items-start w-full items-start m-auto
+               outline-none rounded-md h-6 bg-[#ededed] px-2'
                
               />
         
